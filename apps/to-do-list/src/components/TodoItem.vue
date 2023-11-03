8b41a25dc52fdb846ad0d47f64d6426e750bf852
yuning-dev <yuning.eng@gmail.com>
2023-11-03 15:30:36 +0000

    created presentation of TodoList and TodoItem

---
+++ apps/to-do-list/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="$style.wrapper">
+        <div>Wash dishes</div>
+        <div>
+            <button>Completed</button>
+            <button>Delete</button>
+            <button>Edit</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TodoItem',
+    props: {
+
+    },
+}
+</script>
+
+<style module src="./TodoItem.css" />
---
+++ apps/to-do-list/src/pages/TodoList/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="$style.wrapper">
+        <section :class="[$style.header, $style.card]">
+            <h1 :class="$style.title">
+                Interactive Todo List
+            </h1>
+            <p :class="$style.intro">
+                This TodoList is incredibly addictive. Use with caution,
+                and at your own peril. Mwahahahahah!!!!!??!!!!
+            </p>
+            <div :class="$style.addItemContainer">
+                <input type="text" :class="$style.addItemField">
+                <button>Add list item</button>
+            </div>
+        </section>
+        <section :class="[$style.activeTasksSection, $style.card]">
+            <div :class="$style.listHeader">
+                <h2>
+                    Active tasks
+                </h2>
+                <button>Delete active tasks</button>
+            </div>
+            
+        </section>
+        <section :class="[$style.completedTasksSection, $style.card]">
+            <div :class="$style.listHeader">
+                <h2>
+                    Completed tasks
+                </h2>
+                <button>Delete completed tasks</button>
+            </div>
+            
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+}
+</script>
+
+<style module src="./TodoList.css" />
